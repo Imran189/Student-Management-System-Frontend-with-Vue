e486fdc08115d4c16c0b0c7cc15baf7ca20f4902
Imran189 <imranimu189@gmail.com>
2022-04-16 21:04:12 +0600

    4th commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/pages/HomeView.vue'
+import LoginView from '@/views/auth/LoginView.vue'
 
 const routes = [
   {
     path: '/',
+    name: 'LoginView',
+    component: LoginView
+  },
+  {
+    path: '/admin/home',
     name: 'HomeView',
     component: HomeView
   },
