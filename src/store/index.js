c5560de1cfa19635587dcfd6cd79f9b4ab86d883
Imran189 <imranimu189@gmail.com>
2022-04-18 17:46:19 +0600

    vuex-PersistedState

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore } from 'vuex'
 import {auth} from './modules/auth'
+import createPersistedState from "vuex-persistedstate";
 export default createStore({
   state: {
   },
@@ -11,5 +12,8 @@ export default createStore({
   },
   modules: {
     auth
-  }
+  },
+  plugins: [createPersistedState({
+    paths:['auth']
+  })],
 })
