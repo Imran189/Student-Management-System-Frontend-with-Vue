ef5f74cda5d0aaa919f979530367dce6c22b3a64
Imran189 <imranimu189@gmail.com>
2022-04-20 22:17:40 +0600

    3rd commit

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -170,7 +170,7 @@
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-table"></i>
               <p>
-                Tables
+                Table
                 <i class="fas fa-angle-left right"></i>
               </p>
             </a>
