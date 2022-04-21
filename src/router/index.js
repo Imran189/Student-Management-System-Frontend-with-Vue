16ca0f212c8ac9ab958e61058f9cebaa8cf2e8e8
Imran189 <imranimu189@gmail.com>
2022-04-21 23:29:04 +0600

    router secure

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '@/store'
 import HomeView from '@/views/pages/HomeView.vue'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
@@ -8,22 +9,31 @@ const routes = [
   {
     path: '/',
     name: 'LoginView',
-    component: LoginView
+    component: LoginView,
+    meta:{
+      guest:true
+    }
   },
   {
     path: '/register',
     name: 'RegisterView',
-    component: RegisterView
+    component: RegisterView,
+    meta:{
+      guest:true
+    }
   },
   {
     path: '/forget/pass',
     name: 'ForgetView',
-    component: ForgetView
+    component: ForgetView,
   },
   {
     path: '/admin/home',
     name: 'HomeView',
-    component: HomeView
+    component: HomeView,
+    meta:{
+      requiresAuth:true
+    }
   },
   
 ]
@@ -33,4 +43,23 @@ const router = createRouter({
   routes
 })
 
+router.beforeEach((to) => {
+  // instead of having to check every route record with
+  // to.matched.some(record => record.meta.requiresAuth)
+  if (to.meta.requiresAuth && !store.getters.GET_AUTH_STATUS) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    return {
+      path: '/',
+      
+    }
+  }
+  if (to.meta.guest && store.getters.GET_AUTH_STATUS) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    return {
+      path: '/admin/home',    
+    }
+  }
+})
 export default router
